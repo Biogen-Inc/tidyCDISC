eb8911167f311dd2a2192f7778a7eaa1106d675c
Maya Gans (EXTERNAL) <maya.gans@biogen.com>
2020-01-22 12:37:11 -0800

    cleaning files

--- datafile.R
+++ datafile.R
@@ -6,10 +6,12 @@ library(haven)
 
 dd <- list()
 dd$data <- list(`Example Data` =  "GARBAGE", #"safetyGraphics::adlbc",
-             adsl.sas7bdat = zap_formats(read_sas("adsl.sas7bdat")),
-             advs.sas7bdat = zap_formats(read_sas("advs.sas7bdat")),
-             adex.sas7bdat = zap_formats(read_sas("adex.sas7bdat")))
+             adsl.sas7bdat = zap_formats(read_sas("../../TableGenerator_Data/adsl.sas7bdat")),
+             advs.sas7bdat = zap_formats(read_sas("../../TableGenerator_Data/advs.sas7bdat")),
+             adex.sas7bdat = zap_formats(read_sas("../../TableGenerator_Data/adex.sas7bdat")))
 
 # Your first step will be to remove the example data from your list
 # We'll delete this line of code once I clean the UI to not included the Example
 dd$data[1] <- NULL
+
+lapply(names(dd$data), function(x) assign(x, dd$data[[x]], envir = .GlobalEnv))
