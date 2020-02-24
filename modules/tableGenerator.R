d5047280711201ca066bf9cc2cf1c7a0fad77608
Maya Gans (EXTERNAL) <maya.gans@biogen.com>
2020-02-24 07:17:26 -0800

    adding tg changes

--- modules/tableGenerator.R
+++ modules/tableGenerator.R
@@ -71,29 +71,31 @@ tableGenerator <- function(input, output, session, datafile = reactive(NULL)) {
   
   AGGREGATE <- reactive({
     req(length(input$agg_drop_zone) > 0 & !(is.na(input$agg_drop_zone)))
-    print(input$agg_drop_zone)
     as.data.frame(read_html(input$agg_drop_zone) %>% html_table(fill=TRUE)) %>%
       separate(1, into = c("Aggregate", "Select"), sep=":")
   })
   
   ROWS <- reactive({
     req(length(input$block_drop_zone) > 0 & !(is.na(input$block_drop_zone)))
-    print(input$block_drop_zone)
     as.data.frame(read_html(input$block_drop_zone) %>% html_table(fill=TRUE))
   })
   
   blocks <- reactive({
-    # validate(
-    #   need(nrow(AGGREGATE()) == nrow(ROWS()),  
-    #        if (nrow(AGGREGATE()) < nrow(ROWS())) {
-    #          "Missing Aggregate Block"
-    #        } else {
-    #          "Missing Row Block"
-    #        }))
-    t <- AGGREGATE()
-    p <- ROWS()
-    t$Row <- p$X1
-    return(t)
+
+    if (nrow(AGGREGATE()) != nrow(ROWS())) {
+      if (nrow(AGGREGATE()) < nrow(ROWS())) {
+        stop(call. = FALSE, "Missing statistic block")
+      } else {
+        stop(call. = FALSE, "Missing row block")
+      }
+    } else if (is.na(input$block_drop_zone & is.na(input$agg_drop_zone))) {
+      stop(call. = FALSE, "Add blocks to dropzone to create tables")
+    } else {
+      t <- AGGREGATE()
+      p <- ROWS()
+      t$Row <- p$X1
+      return(t)
+    }
   })
   
   
