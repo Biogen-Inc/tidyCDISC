7c13bccecf58330e46ce19403dcaa0b133875732
Maya Gans (EXTERNAL) <maya.gans@biogen.com>
2020-03-04 13:09:49 -0800

    adding warning message

--- modules/dataUploadUI.R
+++ modules/dataUploadUI.R
@@ -8,13 +8,14 @@ dataUploadUI <- function(id, label = "CSV file") {
              wellPanel(
                h3("Data upload"),
                "Uploading an ADSL file is mandatory, all additional BDS files are optional",
-               fileInput(ns("file"), "Upload a sas7bdat files",accept = c(".sas7bdat"), multiple = TRUE),
+               fileInput(ns("file"), "Upload sas7bdat files",accept = c(".sas7bdat"), multiple = TRUE),
                uiOutput(ns("radio_test"))
              )
       ),
       column(6,
              fluidRow(
                wellPanel(
+                 span(textOutput(ns("multi_studies")), style="color:red;font-size:20px"),
                  uiOutput(ns("datapreview_header")),
                  div(DT::dataTableOutput(ns("data_preview")), style = "font-size: 75%")
                )
