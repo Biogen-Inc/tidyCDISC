f606daf47c4f3c4814612104773a898ede8adafb
Maya Gans (EXTERNAL) <maya.gans@biogen.com>
2020-05-14 10:21:20 -0600

    adding freq block messages

--- R/IDEA_freq.R
+++ R/IDEA_freq.R
@@ -1,7 +1,18 @@
-# perform different operations based on class:
+# perform different mean operations based on class:
 
-IDEA_freq <- function(column, week, group = NULL, data) {
- 
+IDEA_freq <- function(column, week, method, group, data) {
+  UseMethod("IDEA_freq", column)
+}
+
+IDEA_freq.default <- function(column, week, method) {
+  abort(glue(
+    "Can't calculate mean because data is not classified as ADLB, BDS or OCCDS",
+  ))
+}
+
+# if ADSL supplied look for the column to take mean of
+# and look for a grouping variable to group_by
+IDEA_freq.ADSL <- function(column, week, group = NULL, data) {
   column <- sym(as.character(column))
   
   if (is.null(group)) {
@@ -25,4 +36,23 @@ IDEA_freq <- function(column, week, group = NULL, data) {
       select(-n, -prop) %>% 
       spread(!!group, v1)
   }
+}
+
+# if BDS filter by paramcd and week
+# We need to calculate the difference in N for this
+# and report missing values from the mean if any
+IDEA_freq.BDS <- function(column, week, group = NULL, data) {
+  abort(glue(
+    "Can't calculate frequency for BDS - {column} is numeric"
+  ))
+}
+
+IDEA_freq.OCCDS <- function(column, week = NULL) {
+  print("OCCDS")
+}
+
+IDEA_freq.custom <- function(column, week = NULL) {
+  abort(glue(
+    "Can't calculate mean, data is not classified as ADLB, BDS or OCCDS"
+  ))
 }
