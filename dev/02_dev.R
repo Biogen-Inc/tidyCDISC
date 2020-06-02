2e5503c458135d65c0cd563e823cd3eba2f69704
Aaron Clark <aaron.clark@biogen.com>
2020-06-02 09:54:02 -0400

    Added all dependencies except for ideafilter (waiting to sort out a pr) and any tidyverse packages I forgot.

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -15,7 +15,59 @@
 
 ## Dependencies ----
 ## Add one line by package you want to add as dependency
-usethis::use_package( "thinkr" )
+usethis::use_package("shiny") # already added as a recommended package
+usethis::use_package("shinyjs")
+usethis::use_dev_package("tippy") #remotes::install_github("JohnCoene/tippy")
+usethis::use_package("rvest")
+usethis::use_package("haven")
+usethis::use_package("DT") # already added as a recommended package
+usethis::use_package("shinyWidgets")
+usethis::use_package("plotly") 
+usethis::use_package("RColorBrewer")
+usethis::use_package("gridExtra")
+usethis::use_package("grid")
+usethis::use_package("janitor")
+usethis::use_package("shinythemes")
+usethis::use_package("rmarkdown")
+usethis::use_package("shinytest")
+usethis::use_package("waiter")
+usethis::use_package("timevis")
+# usethis::use_package("glue") # already added as a recommended package
+usethis::use_package("sjlabelled") 
+usethis::use_package("data.table")
+usethis::use_package("gt")
+usethis::use_package("shinyBS")
+usethis::use_package("knitr")
+usethis::use_package("rlang")
+usethis::use_package("stringi")
+
+# Error: 'tidyverse' is a meta-package and it is rarely a good idea to depend on it. Please 
+# determine the specific underlying package(s) that offer the function(s) you need and depend 
+# on that instead. For data analysis projects that use a package structure but do not implement
+# a formal R package, adding 'tidyverse' to Depends is a reasonable compromise. Call 
+# `use_package("tidyverse", type = "depends")` to achieve this.
+# usethis::use_package("tidyverse")
+# ran these:
+usethis::use_package("ggplot2")
+usethis::use_package("dplyr")
+usethis::use_package("stringr")
+usethis::use_package("purrr")
+usethis::use_package("forcats")
+usethis::use_package("tidyr")
+usethis::use_package("tibble")
+
+# include these too?
+# usethis::use_package("readr")
+
+# waiting for this to be confirmed working
+# usethis::use_dev_package("IDEAFilter") # devtools::install_github("MayaGans/IDEAFilter")
+
+# don't need anymore
+# usethis::use_package("rtf")
+
+
+
+
 
 ## Add modules ----
 ## Create a module infrastructure in R/
