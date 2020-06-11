e42d6f5dfa2d04aabf85d9e3c3983e8509565bc9
Maya Gans (EXTERNAL) <maya.gans@biogen.com>
2020-06-11 08:57:24 -0700

    addressing bug, reordering functions in tg module

--- R/mod_tableGen.R
+++ R/mod_tableGen.R
@@ -1,67 +1,36 @@
 #' tableGen Server Function
-#' 
-#' This module combines the BDS and ADSL files within the list
-#' of dataframes to create a single data frame.
-#' This data is what's ussed to create the
-#' draggable blocks which appear in a drag zone,
-#' and a custom shiny input binding returns the blocks in the drop zone 
-#' as a list. This list is used to create a gt table output
-#' which can be output as html oor csv.
-#' In order to include OCCDS or custom data the structure of the import data
-#' will need to be changed.
 #'
-#' @param input,output,session Internal parameters for {shiny}
-#' @param datafile all uploaded datafiles from the dataImport module
+#' @param input,output,session 
+#' Internal parameters for {shiny}
+#' @param datafile all uploaded data files 
+#' from the dataImport module
 #' 
 #' @import IDEAFilter
+#' @importFrom rlang sym
+#' @importFrom rlang !!
+#' @importFrom rlang call2
+#' @importFrom rlang as_quosure
 #' @import shiny
 #' @import dplyr
-#' @import gt
-#' @importFrom rlang sym !!
 #' @importFrom purrr map
 #' @importFrom purrr map2
 #' @importFrom purrr imap
+#' @import gt
 #' @importFrom stringi stri_replace_all_regex
 #' @importFrom stringi %s+%
 #' @importFrom glue glue
 #' @importFrom forcats fct_reorder
 #' @import tidyr
 #'
-#' @return the grouped blocks for columns
-#' to be used in the tableGen UI
+#' @noRd 
 
 
 mod_tableGen_server <- function(input, output, session, datafile = reactive(NULL)) {
-  
   
   # ----------------------------------------------------------------------
   # input prep for table manipulation
   # ----------------------------------------------------------------------
   
-  # the reactive column to group data by
-  # change the default value of "NONE" to NULL
-  column <- reactive( if (input$COLUMN == "NONE") NULL else input$COLUMN)
-  
-  # calculate the totals to input after N= in the table headers
-  # a single N if data is not grouped
-  total <- reactive({
-    if (input$COLUMN == "NONE") {
-      all_data() %>% 
-        distinct(USUBJID) %>% 
-        summarise(n = n()) %>%
-        pull(n)
-    } else {
-      all_data() %>%
-        group_by(!!sym(input$COLUMN)) %>%
-        distinct(USUBJID) %>%
-        summarise(n = n()) %>%
-        pull(n)
-    }
-  })
-  
-  # the user can group their data using input$COLUMN
-  # but changing input$recipe will also trigger a group change
-  # in our prespecified demography table we group by TRT01P
   output$col_ADSL <- renderUI({
     x <- input$recipe
     if (is.null(x) | length(x) == 0) { 
@@ -80,21 +49,21 @@ mod_tableGen_server <- function(input, output, session, datafile = reactive(NULL
   # this code will need to be changed if you want to add in OCCDS 
   # ----------------------------------------------------------------------
   
-  # get the ADSL file and all BDS files from the list of dataframes
   ADSL <- reactive({ datafile()$ADSL })
   BDS <- reactive({ datafile()[sapply(datafile(), function(x) "PARAMCD" %in% colnames(x))] })
   
   processed_data <- reactive({ 
     
-    # Seperate ADSL and the PARAMCD dataframes
+    # Seperate ADSL and the PArAMCD dataframes
+    
     PARAMCD <- map(BDS(), ~ if(!"CHG" %in% names(.)) update_list(., CHG = NA) else .)
     
-    # if there are no BDS files 
     if (!is_empty(PARAMCD)) {
       # Bind all the PARAMCD files 
       all_PARAMCD <- bind_rows(PARAMCD, .id = "data_from")  %>% 
         arrange(USUBJID, AVISITN, PARAMCD) %>% 
         select(USUBJID, AVISITN, AVISIT, PARAMCD, AVAL, CHG, data_from)
+      # distinct(USUBJID, AVISITN, AVISIT, PARAMCD, .keep_all = TRUE) 
       
       # Join ADSL and all_PARAMCD
       combined_data <- full_join(ADSL(), all_PARAMCD, by = "USUBJID")
@@ -117,6 +86,40 @@ mod_tableGen_server <- function(input, output, session, datafile = reactive(NULL
   all_data <- callModule(shiny_data_filter, "data_filter", data = processed_data, verbose = FALSE)
   
   
+  # prepare the AVISIT dropdown of the statistics blocks
+  # by converting them to a factor in the order of AVISITN
+  # this allows our dropdown to be in chronological order
+  avisit_words <- reactive({ 
+    req("ADSL" %in% names(datafile()))
+    processed_data()$AVISIT 
+  })
+  avisit_fctr  <- reactive({ 
+    req("ADSL" %in% names(datafile()))
+    processed_data()$AVISITN 
+  })
+  
+  AVISIT <- reactive({
+    req(BDS())
+    
+    if (is.null(avisit_words())) {
+      avisit_words <- " "
+    } else {
+      avisit_words <-
+        tibble(AVISIT = avisit_words(), AVISITN = avisit_fctr()) %>%
+        mutate(AVISIT = as.factor(AVISIT)) %>%
+        mutate(AVISIT = forcats::fct_reorder(AVISIT, AVISITN)) %>%
+        pull(AVISIT) %>%
+        unique()
+    }
+    avisit_words
+  })
+  
+  observe({
+    req(AVISIT())
+    session$sendCustomMessage("my_data", AVISIT())
+  })
+  
+  
   # ----------------------------------------------------------------------
   # Generate table given the droped blocks
   # ----------------------------------------------------------------------
@@ -126,23 +129,23 @@ mod_tableGen_server <- function(input, output, session, datafile = reactive(NULL
     convertTGOutput(input$agg_drop_zone, input$block_drop_zone) 
   })
   
-  # create a lookup table for each stats block
-  # and what to print in the tab headers instead 
-  # of the name of the block:
-  # rather than print STATSBLOCKNAME of COLUMN
-  # replace all STATSBLOCKNAME with a more table friendly label
-  # MEAN becomes Descriptive Statistics etc.
-  block_lookup <- reactive({
-    
-    pretty_blocks <- tibble(
-      Pattern = c("MEAN", "FREQ", "CHG"),
-      Replacement = c("Descriptive Statistics", 
-                      "Summary Counts", 
-                      "Descriptive Statistics of Change from Baseline")
-    ) %>%
-      map(set_names, c("Pattern", "Replacement")) %>%
-      bind_rows() %>%
-      rbind(pretty_blocks)
+  column <- reactive( if (input$COLUMN == "NONE") NULL else input$COLUMN)
+  
+  # calculate the totals to input after N= in the table headers
+  # a single N if data is not grouped
+  total <- reactive({
+    if (input$COLUMN == "NONE") {
+      all_data() %>% 
+        distinct(USUBJID) %>% 
+        summarise(n = n()) %>%
+        pull(n)
+    } else {
+      all_data() %>%
+        group_by(!!sym(input$COLUMN)) %>%
+        distinct(USUBJID) %>%
+        summarise(n = n()) %>%
+        pull(n)
+    }
   })
   
   # create a gt table output by mapping over each row in the block input
@@ -218,6 +221,7 @@ mod_tableGen_server <- function(input, output, session, datafile = reactive(NULL
   
   output$all <- render_gt({ gt_table() })
   
+  
   # ----------------------------------------------------------------------
   # Block UI based on the data files that are uploaded
   # create a two column dataframe of the the ADSL column names
@@ -244,45 +248,31 @@ mod_tableGen_server <- function(input, output, session, datafile = reactive(NULL
     return(new_list)
   })
   
-  # prepare the AVISIT dropdown of the statistics blocks
-  # by converting them to a factor in the order of AVISITN
-  # this allows our dropdown to be in chronological order
-  avisit_words <- reactive({ 
-    req("ADSL" %in% names(datafile()))
-    processed_data()$AVISIT 
-  })
-  avisit_fctr  <- reactive({ 
-    req("ADSL" %in% names(datafile()))
-    processed_data()$AVISITN 
-  })
-  
-  AVISIT <- reactive({
-    req(BDS())
+  # create a lookup table for each stats block
+  # and what to print in the tab headers instead 
+  # of the name of the block:
+  # rather than print STATSBLOCKNAME of COLUMN
+  # replace all STATSBLOCKNAME with a more table friendly label
+  # MEAN becomes Descriptive Statistics etc.
+  block_lookup <- reactive({
     
-    if (is.null(avisit_words())) {
-      avisit_words <- " "
-    } else {
-      avisit_words <-
-        tibble(AVISIT = avisit_words(), AVISITN = avisit_fctr()) %>%
-        mutate(AVISIT = as.factor(AVISIT)) %>%
-        mutate(AVISIT = forcats::fct_reorder(AVISIT, AVISITN)) %>%
-        pull(AVISIT) %>%
-        unique()
-    }
-    avisit_words
+    pretty_blocks <- tibble(
+      Pattern = c("MEAN", "FREQ", "CHG"),
+      Replacement = c("Descriptive Statistics", 
+                      "Summary Counts", 
+                      "Descriptive Statistics of Change from Baseline")
+    )
+    
+    test <- block_data() %>%
+      map(set_names, c("Pattern", "Replacement")) %>%
+      bind_rows() %>%
+      rbind(pretty_blocks)
   })
-  
-  observe({
-    req(AVISIT())
-    session$sendCustomMessage("my_data", AVISIT())
-  })
-
   
   # ----------------------------------------------------------------------
   # Download table
   # Currently CSV and HTML but easy to add more!
   # ----------------------------------------------------------------------
-  
   
   output$download_gt <- downloadHandler(
     filename = function() {
@@ -302,6 +292,5 @@ mod_tableGen_server <- function(input, output, session, datafile = reactive(NULL
   p <- reactive({ rowArea(col = 12, block_data()) })
   
   return(p)
- 
+  
 }
- 
