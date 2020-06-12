38f3e5f55b1e58d659961daba90ae680a32d409f
Aaron Clark <aaron.clark@biogen.com>
2020-06-12 12:17:42 -0400

    data comply finished and half of indvExp

--- R/mod_dataComplyRules.R
+++ R/mod_dataComplyRules.R
@@ -1,15 +1,19 @@
+
 #' dataComplyRules UI Function
 #'
 #' @description A shiny Module.
 #'
-#' @param id,input,output,session Internal parameters for {shiny}.
-#' @param showRules If TRUE, the UI will contain the returned shiny tagList will contain a
-#'   help button and respective modal when clicked. If FALSE, the dataComply module will
-#'   not include a help button to display rules.
-#'   
-#' @noRd 
+#' @param id Internal parameters for {shiny}.
+#' @param showRules If TRUE, the UI will contain the returned shiny tagList will
+#'   contain a help button and respective modal when clicked. If FALSE, the
+#'   dataComply module will not include a help button to display rules.
 #'
 #' @importFrom shiny NS tagList actionButton
+#' 
+#' @return An shiny tagList
+#'
+#' @family dataComply Functions
+#'   
 mod_dataComplyRules_ui <- function(id, showRules = T){
   ns <- NS(id)
   tagList(
@@ -25,17 +29,24 @@ mod_dataComplyRules_ui <- function(id, showRules = T){
     
 #' dataComplyRules Server Function
 #'
-#' Applies situates output from Gather Rules function into a modal condiational
-#'    on clicking "help".
+#' Situates output from gather_rules function in
+#' "mod_dataComplyRules_fct_helpers.R" to a modal condiational on clicking
+#' "help".
 #'
-#' @noRd
-mod_dataComplyRules_server <- function(input, output, session){
+#' @param input,output,session Internal parameters for {shiny}.
+#' @param modal_title A text string to be displayed as the modal's title
+#'   
+#' @import shiny
+#'   
+#' @family dataComply Functions
+#'   
+mod_dataComplyRules_server <- function(input, output, session, modal_title = "ADaM-ish Upload Rules"){
   ns <- session$ns
  
   observeEvent(input$clickRules, {
     
     showModal( modalDialog(
-      title = div(style = "text-align:center; font-weight:bold;","ADaM-ish Upload Rules"),
+      title = div(style = "text-align:center; font-weight:bold;",modal_title),
       footer =
           tagList(
             div(style = "text-align:center; font-size: 12px;",
--- R/mod_indvExp_fct_buildEvents.R
+++ R/mod_indvExp_fct_buildEvents.R
@@ -1,24 +1,30 @@
 
-#' Plot Individual Explorer Visits by Param
+#' Build Events Data Frame
 #'
-#' Gather various shiny inputs from OCCDs datasets (if they exist) and return a single standardized events dataset
+#' Gather various shiny inputs from OCCDs datasets (if they exist) and return a
+#' single standardized events dataset
 #'
-#' @param input_checkbox A Character vector of Events Selected
-#' @param input_apply_filter If \code{TRUE} then toggle dataset used to build events
-#' @param my_loaded_adams A character vector of available dataframes in my_datafile
+#' @param input_checkbox A character vector of event names selected. Some
+#'   possible event names include "Milestones", "Adverse Events", "Con Meds",
+#'   "Labs", "Medical History", etc...
+#' @param input_apply_filter Logical; If \code{TRUE} then toggle data frame used to build
+#'   events to "my_filtered_dat", which is pre-filtered data frame from IDEAFilter
+#' @param my_loaded_adams A character vector of available dataframes in
+#'   my_datafile
 #' @param my_datafile A list of data frames
-#' @param my_filtered_dat The output from IDEA- a filtered dataset 
-#' @param my_usubjid The patient number, in the standard USUBJID format
+#' @param my_filtered_dat The output from IDEAFilter, a pre-filtered dataset
+#' @param my_usubjid Character patient number, in the standard USUBJID format 
 #'
-#'   DO NOT REMOVE.
 #' @import dplyr
 #' @importFrom shinyjs alert
-#' @importFrom tidyr pivot_longer 
+#' @importFrom tidyr pivot_longer
 #' @importFrom purrr map_chr
-#' 
-#' @noRd
-#' 
-
+#'
+#' @return Data frame standardized to include events from vaious OCCDs class
+#'   files
+#'
+#' @family indvExp Functions
+#'   
 build_events <- function(
   input_checkbox
   , input_apply_filter
@@ -27,9 +33,18 @@ build_events <- function(
   , my_filtered_dat
   , my_usubjid
 ){
-  if ("ADAE" %in% my_loaded_adams & "AE" %in% c(input_checkbox)) { # ac: first part not needed?
+  ########
+  # ADAE #
+  ########
+  
+  # If Adverse Events (adae) selected & loaded, and If correct date variable exists
+  # then convert the adae data frame to a standard format.
+  if ("ADAE" %in% my_loaded_adams & "AE" %in% c(input_checkbox)) {
+    
     if("AESTDT" %in% colnames(my_datafile[["ADAE"]])){
-      ae_rec <- (if(input_apply_filter == T) my_datafile[["ADAE"]] %>% semi_join(my_filtered_dat) else my_datafile[["ADAE"]]) %>% 
+      ae_rec <- 
+        # conditionally toggle which dataset is used
+        (if(input_apply_filter == T) my_datafile[["ADAE"]] %>% semi_join(my_filtered_dat) else my_datafile[["ADAE"]]) %>% 
         filter(USUBJID == my_usubjid) %>%
         filter(!is.na(AESTDT)) %>%
         mutate(EVENTTYP = "Adverse Events", DOMAIN = "AE") %>%
@@ -37,8 +52,8 @@ build_events <- function(
         mutate(
           START = AESTDT,
           END = NA,
-          tab_st = ifelse(as.character(START) == "", NA_character_, as.character(START)),
-          tab_en = ifelse(as.character(END) == "", NA_character_, as.character(END)),
+          tab_st = ifelse(as.character(START) == "", NA_character_, as.character(START)), # disp chr in DT
+          tab_en = ifelse(as.character(END) == "", NA_character_, as.character(END)),     # disp chr in DT
           DECODE = paste(AEDECOD, "AESEV:", AESEV, "AESER:", AESER)
         ) %>%
         select(-starts_with("AE")) %>%
@@ -53,6 +68,13 @@ build_events <- function(
     ae_rec <- NULL
   }
   
+  ########
+  # ADSL #
+  ########
+  
+  # If "Milestones" is selected, grab any date variables (var name ends with
+  # "DT") then convert the data frame to a standard format.
+  
   if ("ADSL" %in% my_loaded_adams & "DS" %in% c(input_checkbox)) {
     
     # organizing our ADSL labels for merging below
@@ -60,8 +82,8 @@ build_events <- function(
     n <- ncol(adsl)
     # "label table" for all adsl columns
     labs <- 
-      data.frame(event_var = colnames(adsl)
-                 , DECODE = purrr::map_chr(1:n, function(x) attr(adsl[[x]], "label") )
+      data.frame(event_var = colnames(adsl),
+                 DECODE = purrr::map_chr(1:n, function(x) attr(adsl[[x]], "label") )
       ) %>%
       mutate(event_var = as.character(event_var))
     
@@ -71,20 +93,18 @@ build_events <- function(
       select(USUBJID,ends_with("DT")) %>%
       colnames()
     
-    # cat(paste("\n",adsl_date_cols))
-    
     ds_rec <- (if(input_apply_filter == T) adsl %>% semi_join(my_filtered_dat) else adsl) %>%
       filter(USUBJID == my_usubjid) %>%
       select(all_of(adsl_date_cols)) %>%
       distinct() %>%
-      pivot_longer(-USUBJID, names_to = "event_var", values_to = "START") %>%
+      tidyr::pivot_longer(-USUBJID, names_to = "event_var", values_to = "START") %>%
       subset(!is.na(START)) %>%
-      left_join(labs, by = "event_var") %>% #DECODE variable exists in here
+      left_join(labs, by = "event_var") %>% # DECODE variable exists in "labs"
       arrange(START)%>%
       mutate(EVENTTYP = "Milestones", DOMAIN = "DS",
              END = NA,
-             tab_st = ifelse(as.character(START) == "", NA_character_, as.character(START)),
-             tab_en = ifelse(as.character(END) == "", NA_character_, as.character(END))
+             tab_st = ifelse(as.character(START) == "", NA_character_, as.character(START)), # disp chr in DT
+             tab_en = ifelse(as.character(END) == "", NA_character_, as.character(END))      # disp chr in DT
       ) %>%
       distinct(USUBJID, EVENTTYP, START, END,
                tab_st,
@@ -99,15 +119,17 @@ build_events <- function(
   
   if ("ADCM" %in% my_loaded_adams & "CM" %in% c(input_checkbox)) {
     if("CMSTDT" %in% colnames(my_datafile[["ADCM"]])){
-      cm_rec <- (if(input_apply_filter == T) my_datafile[["ADCM"]] %>% semi_join(my_filtered_dat) else my_datafile[["ADCM"]]) %>%
+      cm_rec <- 
+        # conditionally toggle which dataset is used
+        (if(input_apply_filter == T) my_datafile[["ADCM"]] %>% semi_join(my_filtered_dat) else my_datafile[["ADCM"]]) %>%
         filter(USUBJID == my_usubjid) %>%
         filter(CMDECOD != "") %>%
         mutate(EVENTTYP = "Concomitant Meds", DOMAIN = "CM") %>%
         distinct(USUBJID, EVENTTYP, CMSTDT, CMDECOD, DOMAIN) %>%
         mutate(START = CMSTDT,
                END = NA, 
-               tab_st = ifelse(as.character(START) == "", NA_character_, as.character(START)),
-               tab_en = ifelse(as.character(END) == "", NA_character_, as.character(END)),
+               tab_st = ifelse(as.character(START) == "", NA_character_, as.character(START)), # disp chr in DT
+               tab_en = ifelse(as.character(END) == "", NA_character_, as.character(END)),     # disp chr in DT
                DECODE = CMDECOD) %>%
         select(-starts_with("CM")) %>%
         distinct(.keep_all = TRUE)
@@ -123,14 +145,16 @@ build_events <- function(
   
   if ("ADLB" %in% my_loaded_adams & "LB" %in% c(input_checkbox)) {
     if("LBDT" %in% colnames(my_datafile[["ADLB"]])){
-      lb_rec <- (if(input_apply_filter == T) my_datafile[["ADLB"]] %>% semi_join(my_filtered_dat) else my_datafile[["ADLB"]]) %>%
+      lb_rec <- 
+        # conditionally toggle which dataset is used
+        (if(input_apply_filter == T) my_datafile[["ADLB"]] %>% semi_join(my_filtered_dat) else my_datafile[["ADLB"]]) %>%
         filter(USUBJID == my_usubjid) %>%
         mutate(EVENTTYP = "Lab Results", DOMAIN = "LB") %>%
         distinct(USUBJID, EVENTTYP, LBDT, DOMAIN) %>% # Chris suggested: ADT ANALYSIS DATE, 
         mutate(START = LBDT,
                END = NA,
-               tab_st = ifelse(as.character(START) == "", NA_character_, as.character(START)),
-               tab_en = ifelse(as.character(END) == "", NA_character_, as.character(END)),
+               tab_st = ifelse(as.character(START) == "", NA_character_, as.character(START)), # disp chr in DT
+               tab_en = ifelse(as.character(END) == "", NA_character_, as.character(END)),     # disp chr in DT
                DECODE = "Labs Drawn") %>%
         select(-starts_with("LB")) %>%
         distinct(.keep_all = TRUE)
@@ -147,7 +171,9 @@ build_events <- function(
   if ("ADMH" %in% my_loaded_adams & "MH_" %in% substring(input_checkbox, 1, 3)) {
     # if the date column exists in the data set, build the data
     if("MHSTDTC" %in% colnames(my_datafile[["ADMH"]])){
-      mh_rec <- (if(input_apply_filter == T) my_datafile[["ADMH"]] %>% semi_join(my_filtered_dat) else my_datafile[["ADMH"]]) %>%
+      mh_rec <- 
+        # conditionally toggle which dataset is used
+        (if(input_apply_filter == T) my_datafile[["ADMH"]] %>% semi_join(my_filtered_dat) else my_datafile[["ADMH"]]) %>%
         filter(USUBJID == my_usubjid) %>%
         mutate(EVENTTYP = str_to_title(MHCAT), #used to be "Medical History",
                
@@ -169,12 +195,10 @@ build_events <- function(
                  has_end == F & nchar(MHSTDTC) == 7  ~ paste0(MHSTDTC,"-28"),
                  has_end == F & nchar(MHSTDTC) == 4 ~ paste0(MHSTDTC,"-12-31"),
                  TRUE ~ NA_character_)),
-               tab_st = ifelse(MHSTDTC == "", NA_character_, MHSTDTC),
-               tab_en = ifelse(MHENDTC == "", NA_character_, MHENDTC),
-               # st_imp = ifelse(tab_st == paste(START),0,1),
-               # en_imp = ifelse(tab_en == paste(END),0,1),
+               tab_st = ifelse(MHSTDTC == "", NA_character_, MHSTDTC), # disp chr in DT
+               tab_en = ifelse(MHENDTC == "", NA_character_, MHENDTC), # disp chr in DT
                DECODE = ifelse(is.na(MHDECOD) | MHDECOD == "", MHTERM, MHDECOD),
-               sort_start = ifelse(is.na(START), as.Date("1900-01-01"), START)
+               sort_start = ifelse(is.na(START), as.Date("1900-01-01"), START) # if missing, order those first
         ) %>%
         arrange(sort_start) %>%
         distinct(USUBJID, EVENTTYP, START, END, tab_st, tab_en, DECODE, DOMAIN) %>%
@@ -195,7 +219,7 @@ build_events <- function(
   uni_list <- uni_list[!sapply(uni_list,is.null)]
   
   
-  uni_rec <- #ae_rec %>%
+  uni_rec <- 
     do.call("rbind", uni_list) %>%
     mutate(ord = ifelse(EVENTTYP == "DS", 1, 0),
            sort_start = if_else(is.na(START), as.Date("1900-01-01"), START), # If start is null, show at beginning of table
