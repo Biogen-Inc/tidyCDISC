3184f61907efc182148d2dacfab7789ec9785e3d
Aaron Clark <aaron.clark@biogen.com>
2020-06-17 14:48:22 -0400

    Swapped Phuse for CDISCs data

--- data-raw/adsl.R
+++ data-raw/adsl.R
@@ -1,4 +1,4 @@
 ## code to prepare `adsl` dataset goes here
-adsl <- haven::read_sas("data-raw/adsl.sas7bdat")
+adsl <- haven::read_xpt("data-raw/adsl.xpt")
 
 usethis::use_data(adsl, overwrite = TRUE)
