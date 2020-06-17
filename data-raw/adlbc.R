89e046b2075c97234670740900b5a96751672b3e
Aaron Clark <aaron.clark@biogen.com>
2020-06-17 14:48:22 -0400

    Swapped Phuse for CDISCs data

--- data-raw/adlbc.R
+++ data-raw/adlbc.R
@@ -1,4 +1,4 @@
 ## code to prepare `adlbc` dataset goes here
-adlbc <- haven::read_sas("data-raw/adlbc.sas7bdat")
+adlbc <- haven::read_xpt("data-raw/adlbc.xpt")
 
 usethis::use_data(adlbc, overwrite = TRUE)
