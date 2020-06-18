5d910817fc1152984736740aa0c2bbcf14d28822
Aaron Clark <aaron.clark@biogen.com>
2020-06-18 09:24:46 -0400

    Removed export_all = F from load_all function in app.R so that IDEAFilter doesn't fail. Updated manifest for fun

--- app.R
+++ app.R
@@ -2,7 +2,8 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+# golem default's to these options
+pkgload::load_all(helpers = FALSE, attach_testthat = FALSE) # export_all = FALSE # ac removed: if false, IDEAFilter fails
 options( "golem.app.prod" = TRUE)
 # rsconnect::writeManifest() # ac added
 IDEA::run_app() # add parameters here (if any)
