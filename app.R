183a7b7bb655e41d1fd2772c50022bdc4d53b188
Aaron Clark <aaron.clark@biogen.com>
2020-06-18 09:14:25 -0400

    Added additional pkg authors

--- app.R
+++ app.R
@@ -2,7 +2,7 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
+pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
-rsconnect::writeManifest() # ac added
+# rsconnect::writeManifest() # ac added
 IDEA::run_app() # add parameters here (if any)
