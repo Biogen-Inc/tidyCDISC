36177bdd1c5fe69bdc2af1937f5893d01674a694
Maya Gans (EXTERNAL) <maya.gans@biogen.com>
2020-07-09 12:02:02 -0700

    box plot functionality improved, still need to add weeks and points

--- R/boxplot.R
+++ R/boxplot.R
@@ -17,26 +17,49 @@ boxPlot_srv <- function(input, output, session, data) {
   
   observe({
     req(data())
+    
+    # numeric columns, remove aval, chg, base
     num_col <- subset_colclasses(data(), is.numeric)
-    paramcd <- data()$PARAMCD
+    num_col <- num_col[num_col != "AVAL" | num_col != "CHG" | num_col != "BASE"]
+    
+    # get unique paramcd
+    paramcd <- unique(data()$PARAMCD)
+    
+    group <- subset_colclasses(data(), is.character)
+    group <- group[group != "data_from"]
 
     updateSelectInput(session, "yvar", choices = c(paramcd, num_col))
-    updateSelectInput(session, "group", choices = as.list(subset_colclasses(data(), is.character)))
+    updateSelectInput(session, "group", choices = group)
   })
   
   output$include_var <- renderUI({
     req(input$yvar %in% data()$PARAMCD)
-    shinyWidgets::radioGroupButtons(ns("value"), "Value", choices = c("AVAL", "CHG"))
+    shinyWidgets::radioGroupButtons("value", "Value", choices = c("AVAL", "CHG"))
   })
   
   # -------------------------------------------------
-  # Create boxplot using inputs 
+  # Create boxplot using inputs
   # -------------------------------------------------
   
+  # create plot object using the numeric column on the yaxis
+  # or by filtering the data by PARAMCD, then using AVAL or CHG for the yaxis
   p <- reactive({
-    ggplot2::ggplot(data(), ggplot2::aes_string(x = input$group, y = input$yvar)) +
-      ggplot2::geom_boxplot()
+    if (input$yvar %in% colnames(data())) {
+        p <- ggplot2::ggplot(data()) + 
+          ggplot2::aes(x = !!sym(input$group), y = y()) +
+          ggplot2::geom_boxplot()
+    } else {
+        p <- data() %>% 
+          dplyr::filter(PARAMCD == input$yvar) %>%
+          # dplyr::filter(AVISIT == input$week) %>%
+          ggplot2::ggplot() +
+          ggplot2::aes_string(x = input$group, y = input$value) +
+          ggplot2::geom_boxplot()
+    }
+    #if (input$points) { p <- p + ggplot2::geom_jitter() }
+    return(p)
   })
   
+  # return the plot object to parent module
   return(p)
 }
