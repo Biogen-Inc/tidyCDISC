4ad09cb29f856ab4a57bb9a7f7c6af9abfefc4a9
Maya Gans <jaffe.maya@gmail.com>
2020-07-22 08:33:15 -0700

    updating app.R

--- app.R
+++ app.R
@@ -2,16 +2,8 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-# golem default's to these options
-pkgload::load_all(helpers = FALSE, attach_testthat = FALSE) # export_all = FALSE # ac removed: if false, IDEAFilter fails
+pkgload::load_all(helpers = FALSE,attach_testthat = FALSE) # export_all = FALSE,
 options( "golem.app.prod" = TRUE)
 options(shiny.sanitize.errors = FALSE)
 # rsconnect::writeManifest() # ac added
-IDEA::run_app() # add parameters here (if any)
-# Launch the ShinyApp (Do not remove this comment)
-# To deploy, run: rsconnect::deployApp()
-# Or use the blue button on top of this file
-
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE)
 IDEA::run_app() # add parameters here (if any)
