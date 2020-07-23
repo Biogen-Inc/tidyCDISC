fd07effb9c6678382feef7702aa6a41f7cba7e8c
Maya Gans <maya.gans@biogen.com>
2020-07-23 09:06:31 -0700

    adding files again

--- app.R
+++ app.R
@@ -2,7 +2,8 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-pkgload::load_all(helpers = FALSE,attach_testthat = FALSE) # export_all = FALSE,
+# golem default's to these options
+pkgload::load_all(helpers = FALSE, attach_testthat = FALSE) # export_all = FALSE # ac removed: if false, IDEAFilter fails
 options( "golem.app.prod" = TRUE)
 options(shiny.sanitize.errors = FALSE)
 # rsconnect::writeManifest() # ac added
