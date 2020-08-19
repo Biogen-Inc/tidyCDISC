99d4099c7eabcc7bd134a25db606d46c81290af9
Maya Gans <maya.gans@biogen.com>
2020-08-19 11:01:41 -0700

    helpdesk

--- R/app_ui.R
+++ R/app_ui.R
@@ -32,8 +32,11 @@ app_ui <- function(request) {
                  title = "Individual Explorer",
                  mod_indvExp_ui("indvExp_ui_1")
                )
-      )
-    )
+      ),
+  tags$script(
+    HTML("var header = $('.navbar > .container-fluid');
+                              header.append('<a href=\"mailto:example@email.com\"><img src=\"www/email.svg\" style=\"width:2.5%;height:2.5%;float:right;padding-top:5px;\"></a>')")
+  ))
   }
 
 #' Add external Resources to the Application
