f125e2cb1f7a88b6953d778746d7003890df06b4
Maya Gans <maya.gans@biogen.com>
2020-08-20 09:32:37 -0700

    add dplyr

--- data-raw/advs.R
+++ data-raw/advs.R
@@ -1,6 +1,6 @@
 ## code to prepare `adtte` dataset goes here
 advs <- haven::read_xpt("data-raw/advs.xpt") %>%
-  dplyr::mutate(across(.cols = where(is.character),
+  dplyr::mutate(dplyr::across(.cols = where(is.character),
                        .fns = na_if, y = ""))
 
 usethis::use_data(advs, overwrite = TRUE)
