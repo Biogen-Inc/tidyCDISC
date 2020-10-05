0753fd764757496f8346df99681f421547c78296
Aaron Clark <aaron.clark@biogen.com>
2020-10-05 16:39:11 -0400

    many improvements from a filtering and r script perspective

--- R/utils_SAScompare.R
+++ R/utils_SAScompare.R
@@ -1,25 +1,26 @@
-#' Function to merge the list of user supplied data frames
+#' Function to read the SAS list of user supplied data frames
 #' 
 #' @param datalist list of CDISC dataframes 
 #' 
 #' @export
-combineData <- function(filepaths) {
-  purrr::map(.x, ~haven::read_sas(filepaths))
+readData <- function(filepaths) {
+  purrr::map(filepaths, ~haven::read_sas(.x))
 }
 
 
-#' Function to merge the list of user supplied data frames
+#' Function to bind data rows from the list of user supplied data frames
 #' 
-#' @param datalist list of CDISC dataframes 
+#' @param datafile list of ADaM-ish dataframes 
 #' 
 #' @export
 #' 
-mergeData <- function(datalist) {
-  sasdata0 <- toupper(names(datalist))
-  sasdata <- names(which(sapply(sasdata0,function(df) { return(stringr::str_detect(toupper(df),"^AD[A-Z0-9\\_]+")) })))
+combineData <- function(datafile) {
+  # ac: not needed for this function
+  # sasdata0 <- toupper(names(datalist))
+  # my_loaded_adams <- names(which(sapply(toupper(names(datalist)),function(df) { return(stringr::str_detect(toupper(df),"^AD[A-Z0-9\\_]+")) })))
   
-  ADSL <- reactive({ datafile$ADSL })
-  BDS <- reactive({ datafile[sapply(datafile, function(x) "PARAMCD" %in% colnames(x))] })
+  ADSL <- datafile$ADSL
+  BDS <- datafile[sapply(datafile, function(x) "PARAMCD" %in% colnames(x))]
   
   PARAMCD <- map(BDS, ~ if(!"CHG" %in% names(.)) {update_list(., CHG = NA)} else {.})
   
@@ -36,6 +37,46 @@ mergeData <- function(datalist) {
   }
   return(combined_data)
 }
+
+
+#' Function to filter rows from combined data
+#'
+#' @param datafile list of ADaM-ish dataframes
+#'
+#' @export
+#'
+filterData <- function(datafile, input_filter_df, filter_code) {
+  processed_data <-
+
+    select_dfs <- datafile[input_filter_df]
+
+    # Separate out non BDS and BDS data frames. Note: join may throw some
+    # warnings if labels are different between two datasets, which is fine!
+    # Ignore
+    non_bds <- select_dfs[sapply(select_dfs, function(x) !("PARAMCD" %in% colnames(x)) )]
+    bds <- select_dfs[sapply(select_dfs, function(x) "PARAMCD" %in% colnames(x) )]
+
+    # Make CHG var doesn't exist, create the column and populate with NA
+    PARAMCD_dat <- purrr::map(bds, ~ if(!"CHG" %in% names(.)) {purrr::update_list(., CHG = NA)} else {.})
+
+    # Combine selected data into a 1 usable data frame
+    if (!rlang::is_empty(PARAMCD_dat)) {
+      all_PARAMCD <- bind_rows(PARAMCD_dat, .id = "data_from") %>% distinct(.keep_all = T)
+
+      if (!rlang::is_empty(non_bds)){
+        processed_data <- inner_join(non_bds %>% purrr::reduce(inner_join), all_PARAMCD)
+      } else {
+        processed_data <-all_PARAMCD
+      }
+    } else {
+      processed_data <- non_bds %>% reduce(inner_join)
+    }
+
+    # evaluate the filtered code
+    filtered_data <- eval(filter_code)
+    return(filtered_data)
+}
+
 
 #' Function to compare the SAS table to the IDEA output table
 #' 
