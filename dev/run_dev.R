49d4e2aa003a871e279e4fd73e419a1744dc8d1b
Aaron Clark <aaron.clark@biogen.com>
2020-10-20 10:42:02 -0400

    Initial commit after upgrading to R version 4.0.X

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -10,3 +10,9 @@ golem::document_and_reload()
 # Run the application 
 run_app()
 
+# # devtools::install_github("MayaGans/IDEAFilter")
+# remotes::install_github("MayaGans/IDEAFilter", force = T)
+# remotes::install_github("JohnCoene/tippy", force = T)
+# remotes::install_github("rstudio/rsconnect", force = T)
+# library(installr)
+# install.rstudio() # error
