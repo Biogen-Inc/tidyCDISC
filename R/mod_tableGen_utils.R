9116e4963a6ee87cf678cb6ffa636236d18a5237
Aaron Clark <aaron.clark@biogen.com>
2020-11-19 10:54:06 -0500

    Streamlined the group by column selector

--- R/mod_tableGen_utils.R
+++ R/mod_tableGen_utils.R
@@ -1,14 +1,4 @@
-#' function to change the selected input of input$COLUMN based on the recipe block
-#' 
-#' @param input the shiny input id to change based on the recipe dropdown
-#' @param column the specific column to select within the shiny input
-#' @param dataset the data to use for all choices - ADSL reactive
-#' @import shiny
-#' 
-#' @family tableGen Functions
-recipe_column <- function(input, dataset, column) {
-  selectInput(input, "Group Data By:", choices = c("NONE", colnames(dataset)), selected = column)
-}
+
 
 #' Table Generator Cicerone R6 Object 
 #' 
