ad12163326f3081083c22438fba3b2bfc3ebfcb2
Maya Gans <maya.gans@biogen.com>
2020-11-24 11:52:05 -0800

    commenting js

--- inst/app/www/accordian.js
+++ inst/app/www/accordian.js
@@ -1,3 +1,7 @@
+// this code adds a class "up" to the row palletes
+// and we use that class to turn the arrow up and down
+// and also collapse the block divs
+
 $( document ).ready(function() {
   $(document).on('click', '.accordion', function(){
         /* Select only the accordion clicked on */
