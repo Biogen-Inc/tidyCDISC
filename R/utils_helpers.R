f2e4f20a2ee4390f080c15d7be4f35dda65c5ffe
Aaron Clark <aaron.clark@biogen.com>
2020-12-07 13:41:03 -0500

    sort() was dropping NA values

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -159,7 +159,7 @@ filters_in_english <- function(filtered_data, filter_header = "Filters Applied:"
 #' 
 #' @noRd
 #' 
-getLevels <- function(x) {if(is.factor(x)) levels(x)else sort(unique(x)) } 
+getLevels <- function(x) {if(is.factor(x)) levels(x) else sort(unique(x), na.last = T) } 
 
 
 
