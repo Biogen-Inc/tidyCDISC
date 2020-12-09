d1b9e60c449d5d3e0c7be822c2fa0118a6aeebe6
Aaron Clark <aaron.clark@biogen.com>
2020-12-09 11:10:45 -0500

    Added nested_freq unit tests

---
+++ tests/testthat/test-tableGen_fct_nested_freq.R
@@ -0,0 +1,35 @@
+context("table generator NESTED FREQ stat block")
+
+test_that("NESTED FREQ block on ADSL", {
+  IDEA_nested_freq.ADSL("SEX", "COUNTRY", NULL, tg_data)
+  IDEA_nested_freq.ADSL("SEX", "NONE", NULL, tg_data)
+  IDEA_nested_freq.ADSL("SEX", "COUNTRY", "AVISIT", tg_data)
+  IDEA_nested_freq.ADSL("SEX", "NONE", "AVISIT", tg_data)
+})
+
+test_that("NESTED FREQ block ADSL group", {
+  IDEA_nested_freq.ADSL("ITTFL", "NONE", "COUNTRY", tg_data)
+})
+
+# -------------------------------------------
+# Expected Test Failures
+# -------------------------------------------
+
+test_that("NESTED FREQ MUST have differnt var and grouping variables", {
+  expect_error(IDEA_nested_freq.ADSL("SEX", "NONE", "SEX", data = tg_data))
+})
+test_that("NESTED FREQ block on BDS fails", {
+  expect_error(IDEA_nested_freq.BDS("AGE", "NONE", NULL, tg_data))
+})
+test_that("NESTED FREQ block on NUMERIC column fails", {
+  expect_error(IDEA_nested_freq.ADSL("AGE", "NONE", NULL, tg_data))
+})
+test_that("NESTED FREQ block will return a data frame with num rows equal to each var's unique combinations PLUS the unique levels of the var dragged into left drop zone", {
+  expect_equal(
+    nrow(IDEA_nested_freq.ADSL("SEX", "COUNTRY", NULL, tg_data)),
+    nrow(unique(tg_data[,c("SEX","COUNTRY")])) + length(unique(tg_data$SEX))
+    )
+})
+
+
+
