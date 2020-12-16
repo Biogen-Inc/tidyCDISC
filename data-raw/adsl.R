80672f3dae1f03ef33507d996667f64306d45a91
Aaron Clark <aaron.clark@biogen.com>
2020-12-16 16:40:47 -0500

    Added DTHDT variable with imputed values for patients with a 'Y' value for DTHFL

--- data-raw/adsl.R
+++ data-raw/adsl.R
@@ -1,6 +1,12 @@
 ## code to prepare `adsl` dataset goes here
 adsl <- haven::read_xpt("data-raw/adsl.xpt") %>%
   dplyr::mutate(dplyr::across(.cols = where(is.character),
-                       .fns = na_if, y = ""))
+                       .fns = na_if, y = "")) %>%
+  group_by(USUBJID) %>%
+  mutate(DTHDT = as.Date(ifelse(DTHFL == "Y", 
+                        paste(sample(seq.Date(TRTSDT, TRTEDT, by = 1), size = 1))
+                        , NA_character_)
+                ))
+attr(adsl$DTHDT, "label") <- "Date of Death"
 
 usethis::use_data(adsl, overwrite = TRUE)
