979ef89fbbe8ce3b28a56fa1e7ce7385bf46e6a6
Aaron Clark <aaron.clark@biogen.com>
2020-12-16 17:00:33 -0500

    Adding sas label to adsl column and creating AESEVN column with SAS label and then saving both to an .rda file

--- data-raw/adsl.R
+++ data-raw/adsl.R
@@ -2,11 +2,12 @@
 adsl <- haven::read_xpt("data-raw/adsl.xpt") %>%
   dplyr::mutate(dplyr::across(.cols = where(is.character),
                        .fns = na_if, y = "")) %>%
-  group_by(USUBJID) %>%
-  mutate(DTHDT = as.Date(ifelse(DTHFL == "Y", 
+  dplyr::group_by(USUBJID) %>%
+  dplyr::mutate(DTHDT = as.Date(ifelse(DTHFL == "Y", 
                         paste(sample(seq.Date(TRTSDT, TRTEDT, by = 1), size = 1))
                         , NA_character_)
-                ))
+                )) %>%
+  ungroup()
 attr(adsl$DTHDT, "label") <- "Date of Death"
 
 usethis::use_data(adsl, overwrite = TRUE)
