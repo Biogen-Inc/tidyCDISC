e8361f4e5d347cb6684c7aa48119e8ac1728d2c9
Aaron Clark <aaron.clark@biogen.com>
2020-12-21 14:38:04 -0500

    exported cdisc data sets so that they are usable in R script. Changed the way 'datalist' is created depending on which study is selected on data upload page

--- R/data-phuse_TDF_ADaM.R
+++ R/data-phuse_TDF_ADaM.R
@@ -4,8 +4,8 @@
 #' Subject Level Analysis Dataset from Phuse Test Data Fractory Project's github.
 #'
 #' @source <https://github.com/phuse-org/TestDataFactory/blob/master/Updated/TDF_ADaM/adsl.xpt>, downloaded 2020-06-17
-#'
 #' @format Data frame with 254 features and 51 fields
+#' @export
 #' 
 "adsl"
 
@@ -17,6 +17,7 @@
 #' @source <https://github.com/phuse-org/TestDataFactory/blob/master/Updated/TDF_ADaM/adlbc.xpt>, downloaded 2020-06-17
 #'
 #' @format Data frame with 32,740 features and 58 fields
+#' @export
 #' 
 "adlbc"
 
@@ -28,6 +29,7 @@
 #' @source <https://github.com/phuse-org/TestDataFactory/blob/master/Updated/TDF_ADaM/advs.xpt>, downloaded 2020-06-17
 #'
 #' @format Data frame with 32,139 features and 34 fields
+#' @export
 #' 
 "advs"
 
@@ -39,5 +41,6 @@
 #' @source <https://github.com/phuse-org/TestDataFactory/blob/master/Updated/TDF_ADaM/adae.xpt>, downloaded 2020-06-17
 #'
 #' @format Data frame with 32,139 features and 34 fields
+#' @export
 #' 
 "adae"
