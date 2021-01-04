3000e601cd98ede1e0f4e231e79228ad945e8fb3
Aaron Clark <aaron.clark@biogen.com>
2021-01-04 10:05:43 -0500

    Add a 'DRUG WITHDWRAWN' value for one patient on high dose with a related serious event so that tables 34 and 38 render something.

--- data-raw/adae.R
+++ data-raw/adae.R
@@ -1,5 +1,9 @@
 ## code to prepare `adae` dataset goes here
 adae <- haven::read_xpt("data-raw/adae.xpt") %>%
+  left_join(
+    haven::read_xpt("data-raw/adsl.xpt") %>%
+      distinct(USUBJID, TRT01P, TRT01PN)
+    ) %>%
   dplyr::mutate(dplyr::across(.cols = where(is.character),
                        .fns = na_if, y = "")) %>%
   dplyr::mutate(AESEVN = case_when(AESEV == "MILD" ~ 1,
@@ -8,8 +12,12 @@ adae <- haven::read_xpt("data-raw/adae.xpt") %>%
                                    TRUE ~ NA_real_),
                 AREL = if_else(AEREL %in% c('NOT RELATED', 'UNLIKELY RELATED', 'NONE', 'REMOTE'),
                           'NOT RELATED', 'RELATED'),
-                ARELN = if_else(AREL == 'NOT RELATED', 0, 1)
-  )
+                ARELN = if_else(AREL == 'NOT RELATED', 0, 1),
+                AEACN = if_else(AREL == 'RELATED' & AESER == 'Y' & TRT01P == "Xanomeline High Dose", "DRUG WITHDRAWN", AEACN)
+  ) %>%
+  select(-TRT01P, -TRT01PN)
+
+# unique(adae[,c("AEACN")])
 attr(adae$AESEVN, "label") <- "Severity/Intensity (N)"
 attr(adae$AREL, "label") <- "Analysis Causality"
 attr(adae$ARELN, "label") <- "Analysis Causality (N)"
