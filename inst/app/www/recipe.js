70204fe3d1b216517bb2dce80553dd77f2d15ac6
Aaron Clark <aaron.clark@biogen.com>
2021-02-11 10:49:57 -0500

    tab 41 is populated.

--- inst/app/www/recipe.js
+++ inst/app/www/recipe.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+Shiny.addCustomMessageHandler('adlbc_params', function(bc) {
+  Shiny.addCustomMessageHandler('my_weeks', function(wks) {
+  $(document).ready(function(){
   $(document).on('click', '#RECIPE', function(){
 /* Function to create list of row blocks. If we need a block with a text String
 or a dropdown, make a new function here */
@@ -21,30 +23,51 @@ function selectRecipeBlock(newid, df, values) {
 
 // this is scanning through the array, and appends to the html above 
 // and join() concatenates it all together, joining on nothing ("")
-// the strg (df name) remains constant
-function combineRows(arr, strg) {
+// the df (df name as a string) remains constant
+function combineRows(block_array, df) {
   let t = []
-  arr.forEach(function (e) {
-    t.push(simpleRecipeRowBlock(e, strg))
+  block_array.forEach(function (blk) {
+    t.push(simpleRecipeRowBlock(blk, df))
   });
   t= t.join("")
   return(t)
 }
 
-// this is scanning through the
-function oneAgg_AllOptions(agg_stat, df, select_input) {
+// this similar to combineRows above, but will add additional array into
+// the mix and loops through that too, creating a var block for every combo
+function multiplyCombineRows(var_block, df, select_input) {
   let t = []
-  select_input.forEach(function (sel_input) {
-    t.push(selectRecipeBlock(agg_stat, df, sel_input))
+  var_block.forEach(function (blk) {
+    select_input.forEach(function () {
+      t.push(simpleRecipeRowBlock(blk, df))
+    });
   });
   t= t.join("")
   return(t)
+}
+
+// this is selecting one stat block (that contains a selectInput) 
+// and then scans sets the selection options/choices to only have one
+// value - one stat block per option choices. For example, it can Create
+// a mean block for every week that exists in the study. Helpful for
+// table 41 style tables of lab actuals by param and week or PK style tables
+function oneAgg_eachOption(var_block, agg_stat, df,  select_input) {
+  let t = [];
+  var_block.forEach(function () {
+    select_input.forEach(function (sel_input) {
+      t.push(selectRecipeBlock(agg_stat, df, sel_input));
+    });
+  });
+  t= t.join("");
+  return(t);
 }
 
 
 // These are called arrays
 demography_rows = ["AGEGR1", "AGE",  "SEX",  "ETHNIC", "RACE", "HEIGHTBL", "WEIGHTBL"]
 demography_agg =  ["FREQ",   "MEAN", "FREQ", "FREQ",   "FREQ", "MEAN",     "MEAN"]
+
+
 
 ae18_rows = ["AOCCFL", "AESEV", "AESER","DTHDT"]
 ae18_agg =  ["Y_FREQ", "MAX_FREQ", "Y_FREQ", "NON_MISSING"]
@@ -53,12 +76,11 @@ soc_pt_rows = ["AOCCFL", "AEBODSYS"]
 soc_pt_agg =  ["Y_FREQ", "NESTED_FREQ_DSC"]
 soc_pt_sel = ["NONE", "AEDECOD"]
 
-weeks = Shiny.addCustomMessageHandler('my_weeks', function(x) {
-  Object.values(x) // the dataframe column is imported as an array
-}
-adlbc_params = Shiny.addCustomMessageHandler('adlbc_params', function(x) {
-  Object.values(x) // the dataframe column is imported as an array
-}
+//console.log("initial bc:", wks);
+//bc_params = ["SODIUM","SODIUM","SODIUM"]
+bc_params = Object.values(bc);
+weeks = Object.values(wks);
+
 
 /* Create custom block recipes to automatically populate when selected */
   $("#RECIPE").bind("change", function(event, ui) {
@@ -115,8 +137,6 @@ adlbc_params = Shiny.addCustomMessageHandler('adlbc_params', function(x) {
       $("#droppable_blocks").append($(simpleRecipeRowBlock("AEBODSYS", "ADAE")));
       
       
-      
-      
     } else if (["Table 23: Adverse events by preferred term",
                 "Table 26: Severe adverse events by preferred term",
                 "Table 31: Serious adverse events by preferred term"].includes(publisher)) {
@@ -128,16 +148,23 @@ adlbc_params = Shiny.addCustomMessageHandler('adlbc_params', function(x) {
       $("#droppable_blocks").append($(simpleRecipeRowBlock("AEDECOD", "ADAE")));
       
      } else if (["Table 41: Blood Chemistry actual values by visit"].includes(publisher)) {
+      //document.getElementById("droppable_agg").innerHTML = "";
+      //$("#droppable_agg").append($(combineRows(demography_agg, "ADSL")));
+      //document.getElementById("droppable_blocks").innerHTML = "";
+      //$("#droppable_blocks").append($(combineRows(demography_rows, "ADSL")));
+      
       document.getElementById("droppable_agg").innerHTML = "";
-      $("#droppable_agg").append($(combineRows(demography_agg, "ADSL")));
+      $("#droppable_agg").append($(oneAgg_eachOption(bc_params,"MEAN","ADLBC",weeks)));
       document.getElementById("droppable_blocks").innerHTML = "";
-      $("#droppable_blocks").append($(combineRows(demography_rows, "ADSL")));
+      $("#droppable_blocks").append($(multiplyCombineRows(bc_params, "ADLBC", weeks)));
+
       
     } else {
       document.getElementById("droppable_agg").innerHTML = "";
       document.getElementById("droppable_blocks").innerHTML = "";
     }
   });
+
   
 $('select#RECIPE').change(function() {
   var selectedDropdown = $(this).children('option:selected').val()
@@ -146,3 +173,5 @@ $('select#RECIPE').change(function() {
 
 });
 });
+}); // end of my_weeks
+}); // end of adlbc_params
