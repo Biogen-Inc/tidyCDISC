92b34e582e8587bdf2766b12f2dd1533a5cc9c56
Aaron Clark <aaron.clark@biogen.com>
2021-05-13 09:59:17 -0400

    Added initial line plot... seems to work but will need some fine tuning

---
+++ R/mod_popExp_fct_line.R
@@ -0,0 +1,81 @@
+#' IDEA spaghetti plot
+#' 
+#' Create a spaghetti plot with a time dependent variable as the x-axis
+#' and using either the selected response variable
+#' or if a PARAMCD is selected, then plot the corresponding value. 
+#' Lines are plotted by patient
+#' 
+#' @param data Merged data to be used in plot
+#' @param yvar Selected y-axis 
+#' @param time Selected x-axis constained to time dependent columns
+#' @param value If yvar is a PARAMCD then the user must select 
+#' AVAL, CHG, or BASE to be plotted on the y-axis
+#' 
+#' @family popExp Functions
+IDEA_lineplot <- function(data, yvar, time, value = NULL, separate = "NONE", color = "NONE") {
+  
+  # subset data based on yvar being paramcd or not
+  if (yvar %in% colnames(data)) {
+    suppressWarnings(
+      d0 <- data %>% select(USUBJID, time, value = yvar, one_of(color, separate))
+    )
+    yvar_label <- yl <- attr(d0[[yvar]], "label")
+  } else {
+    d0 <- data %>% dplyr::filter(PARAMCD == yvar) %>% select(USUBJID, time, value, one_of(color, separate))
+    yvar_label <- paste(unique(d0$PARAM))
+    yl <- glue::glue("{yvar_label} ({attr(d0[[value]], 'label')})")
+  }
+  xl <- attr(d0[[time]], "label")
+  
+  
+  # by <- sym("cyl")
+  # mtcars %>%
+  #   # group_by(!!by, gear) %>%
+  #   group_by_at(vars(one_of("none"), gear)) %>%
+  #   summarize(mean_mpg = mean(mpg))
+  
+  value_sym <- rlang::sym(value)
+  
+  # Group data as needed to calc means
+  print(d0)
+  d <-
+    d0 %>%
+    group_by_at(vars(time, one_of(color, separate))) %>%
+    summarize(MEAN = round(mean(!!value_sym, na.rm = T), 2),
+              SEM = round(std_err(!!value_sym, na.rm = T),2),
+              N = n_distinct(USUBJID, na.rm = T),
+              # n = n(),
+              .groups = "keep") %>%
+    ungroup() %>%
+    mutate(lower = MEAN - SEM, upper = MEAN + SEM)
+  print(d)
+  
+  
+  # if separate or color used, include those "by" variables in title
+  var_title <- paste(yvar_label, "by", xl)
+  by_title <- case_when(
+    separate != "NONE" & color != "NONE" ~ paste("\nby", attr(data[[color]], "label"), "and", attr(data[[separate]], "label")),
+    separate != "NONE" ~ paste("\nby", attr(data[[separate]], "label")),
+    color != "NONE" ~ paste("\nby", attr(data[[color]], "label")), 
+    TRUE ~ ""
+  )
+  
+  # Add common layers to plot
+  p <- d %>%
+    ggplot2::ggplot() +
+    ggplot2::aes_string(x = time, y = "MEAN")  +
+    ggplot2::geom_line() +
+    ggplot2::geom_point(na.rm = TRUE) +
+    ggplot2::labs(x = xl, y = yl, title = paste(var_title, by_title)) +
+    ggplot2::theme_bw() +
+    ggplot2::theme(text = element_text(size = 12),
+                   axis.text = element_text(size = 12),
+                   plot.title = element_text(size = 16))
+  
+  # Add in plot layers conditional upon user selection
+  if (separate != "NONE") { p <- p + ggplot2::facet_wrap(stats::as.formula(paste(".~", separate))) }
+  if (color != "NONE") { p <- p + ggplot2::aes_string(color = color)}
+  if (by_title != "") {p <- p + theme(plot.margin = margin(t = 1.2, unit = "cm"))}
+  
+  return(p)
+}
---
+++ R/mod_popExp_line.R
@@ -0,0 +1,134 @@
+#' Spaghetti Plot UI
+#'
+#' This module contains the widgets needed to create
+#' a spaghetti plot
+#'
+#' @param id module ID
+#' @param label module label
+#'
+#' @import shiny
+#' @import dplyr
+#'
+#' @family popExp Functions
+#'  
+linePlot_ui <- function(id, label = "spaghetti") {
+  ns <- NS(id)
+  tagList(
+    h4("Select axes:"),
+    wellPanel(
+      selectInput(ns("yvar"), "Select y-axis", choices = NULL),
+      fluidRow(column(12, align = "center", uiOutput(ns("include_var")))),
+      selectInput(ns("time"), "Time Variable", choices = NULL)
+      ),
+    h4("Group data:"),
+    wellPanel(
+      selectInput(ns("separate"), "Separate Plots By", choices = "NONE", selected = "NONE"),
+      selectInput(ns("color"), "Color Plots By", choices = "NONE", selected = "NONE")
+    )
+  )
+}
+
+#' Spaghetti Plot Server Function
+#'
+#' Using the widgets from the spaghetti plot UI
+#' create a ggplot object which is returned to the 
+#' parent Population Explorer module
+#'
+#' @param input,output,session Internal parameters for {shiny}.
+#' @param data The combined dataframe from population explorer
+#'
+#' @import shiny
+#' @import dplyr
+#'
+#' @return ggplot object
+#'
+#' @family popExp Functions
+#'  
+linePlot_srv <- function(input, output, session, data) {
+  ns <- session$ns
+  
+  # -------------------------------------------------
+  # Update Inputs
+  # -------------------------------------------------
+  
+  observe({
+    req(data())
+    
+    # get time based column names
+    seltime_init <- sort(colnames(dplyr::select(data(), ends_with("DY"), contains("VIS"))))
+    
+    # numeric columns, remove aval, chg, base
+    # then remove the x-axis selectors
+    num_col <- subset_colclasses(data(), is.numeric)
+    num_col <- num_col[num_col != "AVAL" & num_col != "CHG" & num_col != "BASE"]
+    num_col <- sort(c(setdiff(seltime_init, num_col), setdiff(num_col, seltime_init)))
+    
+    # add paramcds to y-axis options
+    paramcd <- sort(na.omit(unique(data()$PARAMCD)))
+    
+    updateSelectInput(session, "yvar",
+                      choices = list(`Time Dependent` = paramcd,`Time Independent` = num_col),
+                      selected = isolate(input$yvar))
+    
+    # Update time variable based on yvar selection
+    if(input$yvar != "" & !(input$yvar %in% colnames(data()))){
+      seltime <- data() %>%
+        dplyr::filter(PARAMCD == input$yvar) %>% # subset data
+        select_if(~!all(is.na(.))) %>%
+        dplyr::select(ends_with("DY"), contains("VIS")) %>% # grab time vars remaining
+        colnames() %>% sort()
+    } else {
+      seltime <- seltime_init
+    }
+    updateSelectInput(session, "time", choices = seltime, selected = isolate(input$time))
+  })
+  
+  output$include_var <- renderUI({
+    req(input$yvar %in% data()$PARAMCD)
+    shinyWidgets::radioGroupButtons(ns("value"), "Value",
+                                    choices = c("AVAL", "CHG", "BASE"),
+                                    selected = isolate(input$value)
+                                    )
+  })
+  
+  observeEvent(list(input$yvar), {
+    req(input$yvar != "")
+    
+    # Update grouping variable based on yvar selection
+    if(!(input$yvar %in% colnames(data())) ){ # yvar paramcd #& input$xvar %in% colnames(data())
+      group_dat <- data() %>% 
+        dplyr::filter(PARAMCD == input$yvar) %>% 
+        select_if(~!all(is.na(.))) # remove NA cols
+    } else {
+      group_dat <- data()
+    } 
+    
+    # character and factor columns for coloring or separating
+    char_col <- subset_colclasses(group_dat, is.character)
+    fac_col <- subset_colclasses(group_dat, is.factor)
+    group <- sort(c(fac_col, char_col))
+    
+    # populate dropdowns with choices
+    updateSelectInput(session, "color",
+                      choices = c("NONE", group),
+                      selected = isolate(input$color))
+    updateSelectInput(session, "separate",
+                      choices = c("NONE", group),
+                      selected = isolate(input$separate))
+    
+  })
+  
+  # -------------------------------------------------
+  # Create plot using inputs
+  # -------------------------------------------------
+  
+  # create plot object using the numeric column on the yaxis
+  # or by filtering the data by PARAMCD, then using AVAL or CHG for the yaxis
+  p <- reactive({
+    req(data(), input$yvar, input$time)
+    IDEA_lineplot(data(), input$yvar, input$time, input$value, input$separate, input$color)
+  })
+  
+  # return the plot object to parent module
+  return(p)
+}
