3792d64cada2c81bc27ebd3d4983f1e7040f289a
Aaron Clark <aaron.clark@biogen.com>
2022-01-07 14:45:50 -0500

    updated app.R and deployed

--- app.R
+++ app.R
@@ -10,17 +10,6 @@ options(shiny.sanitize.errors = FALSE)
 tidyCDISC::run_app() # add parameters here (if any)
 
 # Test IDEAFilter
-
-
-
-# # Install internal version from RSPM's GHE repo:
-# options(repos = c(
-#   CRAN = "https://cran.rstudio.com/",
-#   ghe = "http://10.240.22.159:4242/Git-Biogen/latest")
-# )
-# options('repos') # to confirm "ghe" was added
-# install.packages("IDEAFilter")
-
 # # Install external version from Biogen's external repo:
 # remotes::install_github("Biogen-Inc/IDEAFilter")
 # devtools::install_github("Biogen-Inc/IDEAFilter")
