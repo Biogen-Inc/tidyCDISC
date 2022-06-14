e29ceb93465c86d2c208b2d4019df8b340fce4cc
Jeff Thompson <jeff.thompson1@biogen.com>
2022-06-14 13:44:25 -0400

    Remove `deliver_avals_by()`

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -332,17 +332,3 @@ varN_fctr_reorder2 <- function(data) {
 }
 
 
-
-#' Deliver 'avals_by' value to the parent frame/ env
-#' 
-#' @param x a value to assign to `avals_by` in the parent env
-#' 
-#' @export
-#' @keywords internal
-#' 
-deliver_avals_by <- function(x) {
-  with(parent.frame(), {avals_by <- x })
-}
-
-
-
