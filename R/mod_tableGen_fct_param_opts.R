ff959a23f3bf0cd96ca3ad9b03b2b2d344303183
Jeff Thompson <jeff.thompson1@biogen.com>
2023-02-24 12:51:22 -0500

    Add S3 methods to parse recipes

---
+++ R/mod_tableGen_fct_param_opts.R
@@ -0,0 +1,45 @@
+stat_options <- function(block, datalist, ...) {
+  UseMethod("stat_options", block)
+}
+
+stat_options.default <- function(block, datalist, ...) {
+  block$stat_options <- block$stat_arg
+  
+  block
+}
+
+stat_options.avisit <- function(block, datalist, ...) {
+  avisits <- 
+    datalist[[block$data]] %>%
+    dplyr::filter(stringr::str_detect(toupper(AVISIT), "UNSCHEDULED", negate = TRUE)) %>%
+    dplyr::distinct(AVISIT, AVISITN) %>%
+    varN_fctr_reorder() %>%
+    dplyr::pull(AVISIT) %>%
+    get_levels() %>%
+    as.list()
+  
+  block$stat_options <- avisits
+  
+  block
+}
+
+stat_options.avisit_lab <- function(block, datalist, ...) {
+  avisits <-
+    datalist[sapply(datalist, function(x) "PARAMCD" %in% colnames(x)) & substr(names(datalist), 1, 4) == "ADLB"] %>%
+    purrr::map_dfc(dplyr::select, PARAMCD, AVAL, AVISIT, AVISITN) %>%
+    dplyr::filter(PARAMCD == block$variable) %>%
+    dplyr::filter(!is.na(AVAL),
+             !is.na(AVISIT),
+             !(AVISIT %in% c(" ", "")),
+             stringr::str_detect(toupper(AVISIT),"UNSCHEDULED",negate = TRUE)
+    ) %>%
+    dplyr::distinct(AVISIT, AVISITN) %>%
+    varN_fctr_reorder() %>%
+    dplyr::pull(AVISIT) %>%
+    get_levels() %>%
+    as.list()
+    
+  block$stat_options <- avisits
+  
+  block
+}
