11e4984113455749bde21eae4a3e542d16e44c9e
Jeff Thompson <jeff.thompson51317@gmail.com>
2023-03-30 16:43:53 -0400

    Create options group if block options are an object

--- inst/app/www/recipe.js
+++ inst/app/www/recipe.js
@@ -3,7 +3,20 @@ $(document).ready(function(){
 /* Function to create list of row blocks. If we need a block with a text String
 or a dropdown, make a new function here */
 function createRecipeBlock(newid, df, selection, options) {
-  let values = Array.isArray(options) ? options.map(createOption).join("") : ''
+  let values = ''
+  if (Array.isArray(options)) {
+    values = options.map(createOption).join("")
+  } else if (typeof options === "object") {
+    for (i in options) {
+      values = ['']
+      values.push("<optgroup label='" + i + "'>");
+      values.push($.map(options[i], createOption).join(""));
+      values.push("</optgroup>");
+    }
+  } else {
+    values = ''
+  }
+  //Array.isArray(options) ? options.map(createOption).join("") : ''
   return `<div class="form-group drop_area">` +
   `<label class="control-label ${df}" for="${newid}">${newid}</label>` +
   (selection === undefined ? `` : `<select id="${newid}" class="dropdown">` +
