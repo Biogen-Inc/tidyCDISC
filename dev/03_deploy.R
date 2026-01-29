a04e10e3928eba483ed3c5baeae4107c03a8c373
Ryan-W-Harrison <harrison.ryan.w@gmail.com>
2026-01-28 20:57:48 -0500

    * Update dependencies * Add manifest for deployment * Update NAMESPACE * Update documentation * Add manifest.json to .Rbuildignore

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -1,12 +1,12 @@
 # Building a Prod-Ready, Robust Shiny Application.
-# 
-# README: each step of the dev files is optional, and you don't have to 
-# fill every dev scripts before getting started. 
-# 01_start.R should be filled at start. 
+#
+# README: each step of the dev files is optional, and you don't have to
+# fill every dev scripts before getting started.
+# 01_start.R should be filled at start.
 # 02_dev.R should be used to keep track of your development during the project.
 # 03_deploy.R should be used once you need to deploy your app.
-# 
-# 
+#
+#
 ######################################
 #### CURRENT FILE: DEPLOY SCRIPT #####
 ######################################
@@ -35,3 +35,5 @@ golem::add_dockerfile_shinyproxy()
 
 ## If you want to deploy to Heroku
 golem::add_dockerfile_heroku()
+
+rsconnect::writeManifest()
--- dev/run_dev.R
+++ dev/run_dev.R
@@ -1,5 +1,9 @@
 # Sass code compilation
-sass::sass(input = sass::sass_file("inst/app/www/timevis.sass"), output = "inst/app/www/timevis.css", cache = NULL)
+sass::sass(
+  input = sass::sass_file("inst/app/www/timevis.sass"),
+  output = "inst/app/www/timevis.css",
+  cache = NULL
+)
 
 # Set options here
 options(golem.app.prod = FALSE) # TRUE = production mode, FALSE = development mode
@@ -12,7 +16,7 @@ golem::detach_all_attached()
 # Document and reload your package, which runs these three functions...
 golem::document_and_reload()
 
-# Run the application 
+# Run the application
 run_app(launch.browser = TRUE)
 
 # # turn off any options
@@ -25,5 +29,3 @@ run_app(launch.browser = TRUE)
 #     run_app(launch.browser = TRUE)
 #   )
 # })
-
-
